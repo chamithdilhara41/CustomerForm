c3764cce05e086d5bf5d01dc1112a35e74812160
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-02-15 11:06:14 +0530

    first commit

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/main_Form.fxml"))));
+        stage.setTitle("Main Form");
+
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
